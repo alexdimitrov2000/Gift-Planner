7cfa289067f821a9cd4dcd3efdc64d2a1fc68710
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-12-07 15:21:39 +0200

    Finished the design of the login page.

--- front-end/src/components/Login/Login.jsx
+++ front-end/src/components/Login/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import './Login.css';
+import '../shared/styles/loginAndRegister.css';
 
 const Login = () => {
-    return <div className="body">Login Page</div>;
+    return <div className="login-page">
+        <div className="login-form">
+            <form action="/login" method="POST">
+                <h1 className="page-title">Login</h1>
+
+                <div className="form-control">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+
+                <div className="submit-button">
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>;
 }
 
 export default Login;
