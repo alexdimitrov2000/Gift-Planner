da88cc9f7b000bcf63a8121f920005db47592591
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-12-13 23:45:46 +0200

    Added a ProtectedRoute component and protected most of the routes of the application.

--- front-end/src/components/App/App.jsx
+++ front-end/src/components/App/App.jsx
@@ -13,6 +13,8 @@ const Login = React.lazy(() => import('../Login/Login'));
 const Logout = React.lazy(() => import('../Logout/Logout'));
 const Register = React.lazy(() => import('../Register/Register'));
 const Profile = React.lazy(() => import('../Profile/Profile'));
+const CreateGift = React.lazy(() => import('../Gifts/CreateGift/CreateGift'));
+const ProtectedRoute = React.lazy(() => import('../ProtectedRoute/ProtectedRoute'));
 
 class App extends React.Component {
   constructor(props) {
@@ -37,19 +39,22 @@ class App extends React.Component {
   }
 
   render() {
+    const { isLogged } = this.state;
+
     return (
       <BrowserRouter>
         <div className="site">
-          <Header isLogged={this.state.isLogged} />
+          <Header isLogged={isLogged} />
           <Lights />
           <div className="site-main">
             <Switch>
               <Suspense fallback={<Loader className="loader" type="spinningBubbles" color="#0F8A5F" width="12rem" height="12rem" />}>
                 <Route path="/" exact component={Home} />
-                <Route path="/login" render={(props) => <Login {...props} login={this.login} />} />
-                <Route path="/logout" render={(props) => <Logout {...props} logout={this.logout} />} />
-                <Route path="/register" component={Register} />
-                <Route path="/profile" component={Profile} />
+                <ProtectedRoute redirectTo="/" isLogged={!isLogged} path="/login" exact render={(props) => <Login {...props} login={this.login} />} />
+                <ProtectedRoute redirectTo="/" isLogged={!isLogged} path="/register" exact component={Register} />
+                <ProtectedRoute redirectTo="/login" isLogged={isLogged} path="/logout" exact render={(props) => <Logout {...props} logout={this.logout} />} />
+                <ProtectedRoute redirectTo="/login" isLogged={isLogged} path="/profile" exact component={Profile} />
+                <ProtectedRoute redirectTo="/login" isLogged={isLogged} path="/gifts/create" exact component={CreateGift} />
               </Suspense>
             </Switch>
           </div>
