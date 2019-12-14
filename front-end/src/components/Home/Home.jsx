9a6c232c13deb58ca7068985d728c1bd2c51dc1f
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-12-14 17:52:34 +0200

    Edited the home page.

--- front-end/src/components/Home/Home.jsx
+++ front-end/src/components/Home/Home.jsx
@@ -10,6 +10,10 @@ const Home = () => {
                 <p className="secondary-greet">Surprise your family and friends</p>
             </div>
         </div>
+        
+        <header>
+            <h1 className="page-title">Top 3 Gifts</h1>
+        </header>
         <Gifts limit={3} />
     </div>
 };
