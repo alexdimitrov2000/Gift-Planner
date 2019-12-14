17ffa782abf56962ff4a9c112feed9880a182d1c
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-12-14 17:47:01 +0200

    Increased the expire time of the JSON Web Token.

--- back-end/utils/jwt.js
+++ back-end/utils/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const secret = 'shhhhh';
 
 function createToken(data) {
-    return jwt.sign(data, secret, { expiresIn: '1h' });
+    return jwt.sign(data, secret, { expiresIn: '3h' });
 }
 
 function verifyToken(token) {
