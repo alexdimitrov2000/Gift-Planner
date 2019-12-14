4e8d087d0533a765abb62db2e8a7098055bd6dee
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-12-14 14:11:28 +0200

    Replaced Links with NavLinks so that active styles can be applied.

--- front-end/src/components/Navigation/Navigation.jsx
+++ front-end/src/components/Navigation/Navigation.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Navigation.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Navigation({ isLogged }) {
     return <nav className="site-nav">
         {isLogged ?
             <ul>
-                <li><Link to="/gifts/create">Create Gift</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
+                <li><NavLink to="/gifts/create" activeClassName="active">Create Gift</NavLink></li>
+                <li><NavLink to="/profile" activeClassName="active">Profile</NavLink></li>
                 <li><Link to="/logout">Logout</Link></li>
             </ul>
             :
             <ul>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Register</Link></li>
+                <li><NavLink to="/login" activeClassName="active">Login</NavLink></li>
+                <li><NavLink to="/register" activeClassName="active">Register</NavLink></li>
             </ul>
         }
     </nav>;
